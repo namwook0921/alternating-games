f69c2fa5c83c7c38333c0f815e29a778156ed578
namwook0921 <namwook0921@gmail.com>
2024-08-19 19:46:10 +0000

    .

--- iLQR.m
+++ iLQR.m
@@ -28,7 +28,7 @@ function [X_array, X_prime_array, U1_array, U2_array, L1, L2] = ...
 
     n = 1;
 
-    initial_eta = 1;
+    initial_eta = eta;
     eta_step = 0.5;
     max_iter = 5;
 
@@ -36,6 +36,21 @@ function [X_array, X_prime_array, U1_array, U2_array, L1, L2] = ...
 
     B1_dummy = zeros(size(B1_array(:, :, 1)));
     B2_dummy = zeros(size(B1_array(:, :, 1)));
+
+
+    A_jacobian = jacobian(f, x);
+    B1_jacobian = jacobian(f, u1);
+    B2_jacobian = jacobian(f, u2);
+
+    G1_x = gradient(g1, x);
+    G1_u = gradient(g1, u1);
+    H1_x = hessian(g1, x);
+    H1_u = hessian(g1, u1);
+
+    G2_x = gradient(g2, x);
+    G2_u = gradient(g2, u2);
+    H2_x = hessian(g2, x);
+    H2_u = hessian(g2, u2);
 
 
     % Initial trajectory when there is no control
@@ -47,7 +62,8 @@ function [X_array, X_prime_array, U1_array, U2_array, L1, L2] = ...
         disp(n)
         n = n + 1;
         
-        if mod(n, 5) == 1
+        if mod(n, 5) == 0
+        % if true
             % Graph of nth trajectory
             x1 = squeeze(X_array(1, 1, :));
             y1 = squeeze(X_array(2, 1, :));
@@ -71,52 +87,31 @@ function [X_array, X_prime_array, U1_array, U2_array, L1, L2] = ...
         
         % Linearize the System (update A, B arrays)
         [A_array(:, :, 1), B1_array(:, :, 1), B2_dummy] = ...
-            linearizeSystem(f, x, u1, u2, X_array(:, :, 1), X_prime_array(:, :, 1), U1_array(:, :, 1), first_U2);
+            linearizeSystem(A_jacobian, B1_jacobian, B2_jacobian, x, u1, u2, X_array(:, :, 1), X_prime_array(:, :, 1), U1_array(:, :, 1), first_U2);
         [A_prime_array(:, :, 1), B1_dummy, B2_array(:, :, 1)] = ...
-            linearizeSystem(f, x, u1, u2, X_array(:, :, 1), X_prime_array(:, :, 1), U1_array(:, :, 1), U2_array(:, :, 1));
+            linearizeSystem(A_jacobian, B1_jacobian, B2_jacobian, x, u1, u2, X_array(:, :, 1), X_prime_array(:, :, 1), U1_array(:, :, 1), U2_array(:, :, 1));
+        
        
 
         for i = 2 : T
             [A_array(:, :, i), B1_array(:, :, i), B2_array(:, :, i)] = ...
-                linearizeSystem(f, x, u1, u2, X_array(:, :, i), X_prime_array(:, :, i), U1_array(:, :, i), U2_array(:, :, i - 1));
+                linearizeSystem(A_jacobian, B1_jacobian, B2_jacobian, x, u1, u2, X_array(:, :, i), X_prime_array(:, :, i), U1_array(:, :, i), U2_array(:, :, i - 1));
             [A_prime_array(:, :, i), B1_array(:, :, i), B2_array(:, :, i)] = ...
-                linearizeSystem(f, x, u1, u2, X_array(:, :, i), X_prime_array(:, :, i), U1_array(:, :, i), U2_array(:, :, i));
+                linearizeSystem(A_jacobian, B1_jacobian, B2_jacobian, x, u1, u2, X_array(:, :, i), X_prime_array(:, :, i), U1_array(:, :, i), U2_array(:, :, i));
         end
     
 
         % Quadraticize the costs (update Q, R arrays)
         for i = 1 : T
-            [Q1_array(:, :, i), R1_array(:, :, i), q1_array(:, :, i), r1_array(:, :, i)] = quadraticizeCost(g1, x, u1, X_prime_array(:, :, i), U1_array(:, :, i));
-            [Q2_array(:, :, i), R2_array(:, :, i), q2_array(:, :, i), r2_array(:, :, i)] = quadraticizeCost(g2, x, u2, X_array(:, :, i + 1), U2_array(:, :, i));
+            [Q1_array(:, :, i), R1_array(:, :, i), q1_array(:, :, i), r1_array(:, :, i)] = quadraticizeCost(G1_x, G1_u, H1_x, H1_u, x, u1, X_prime_array(:, :, i), U1_array(:, :, i));
+            [Q2_array(:, :, i), R2_array(:, :, i), q2_array(:, :, i), r2_array(:, :, i)] = quadraticizeCost(G2_x, G2_u, H2_x, H2_u, x, u2, X_array(:, :, i + 1), U2_array(:, :, i));
         end
         
-        % disp(Q1_array(:, :, 30));
-        % 
-        % disp(R1_array(:, :, 30));
-        % 
-        % disp(q1_array(:, :, 30));
-        % 
-        % disp(r1_array(:, :, 30));
        
         % Find the solution on delta X
         [delta_X_array, delta_X_prime_array, S1_array, S2_array, T1_array, T2_array, delta_U1_array, delta_U2_array, alpha1_array, alpha2_array, L1, L2] ...
         = lq_cost_solution(first_X, first_B2, first_U2, A_array, A_prime_array, B1_array, B2_array, Q1_array, Q2_array, R1_array, R2_array, q1_array, q2_array, r1_array, r2_array, T);
         
-        % disp(alpha1_array);
-
-        % Display the delta X solution
-        % I suspect it is inaccurate but cannot find the reason.
-        % x1_vector = squeeze(delta_X_array(1, 1, :));
-        % x2_vector = squeeze(delta_X_array(5, 1, :));
-        % 
-        % time_frame = 1 : T + 1;
-        % 
-        % figure;
-        % 
-        % plot(time_frame, x1_vector, 'b', time_frame, x2_vector, 'r');
-        % 
-        % xlabel('time');
-        % legend('Player 1', 'Player 2');
         
         
         % Start from same initial state, then update the trajectory based
@@ -132,7 +127,6 @@ function [X_array, X_prime_array, U1_array, U2_array, L1, L2] = ...
     
             new_X_prime_array(:, :, 1) = double(subs(f, allVars, [new_X_array(:, :, 1); new_U1_array(:, :, 1); first_U2]));
     
-        
             new_U2_array(:, :, 1) = U2_array(:, :, 1) - S2_array(:, :, 1) * (new_X_prime_array(:, :, 1) - X_prime_array(:, :, 1)) ... 
                 - T2_array(:, :, 1) * (new_U1_array(:, :, 1) - U1_array(:, :, 1)) - eta * alpha2_array(:, :, 1);
     
@@ -148,8 +142,8 @@ function [X_array, X_prime_array, U1_array, U2_array, L1, L2] = ...
                 new_X_array(:, :, i + 1) = subs(f, allVars, [new_X_prime_array(:, :, i); new_U1_array(:, :, i); new_U2_array(:, :, i)]);
             end
     
-
             step_converged = true;
+
             for i = 1:T
                 if norm(new_X_array(:, :, i) - X_array(:, :, i)) > step_threshold
                     step_converged = false;
